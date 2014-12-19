f11267f9a846db35173289023e7906a73fdf56ce
Akash <akashkurdekar@gmail.com>
2014-12-19 21:06:56 +0000

    added jshint with stylish

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,15 @@
 var gulp = require('gulp');
 var jasmine = require('gulp-jasmine');
+var jshint = require('gulp-jshint');
+var stylish = require('jshint-stylish');
 
-gulp.task('test', function () {
+gulp.task('jshint', function () {
+  gulp.src('*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter(stylish));
+});
+
+gulp.task('test', ['jshint'], function () {
   return gulp.src('*.spec.js')
     .pipe(jasmine());
 });
